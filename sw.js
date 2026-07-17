059a595b7ccd6fd29a7cc15bb219dac32a848fb9
Emil <mihov.emil@gmail.com>
2026-07-17 23:53:26 +0300

    sw v3: cache study.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='scrumhack-v2';
-const A=['./','./index.html','./manifest.json','./icon.svg','./js/cards.js','./js/questions.js','./js/core.js','./js/app.js'];
+const C='scrumhack-v3';
+const A=['./','./index.html','./manifest.json','./icon.svg','./js/cards.js','./js/questions.js','./js/study.js','./js/core.js','./js/app.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
